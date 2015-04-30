d998aec7f2a57cd3fa168ce58538a5577ac76aea
Burgett, Gordon <Gordon.Burgett@concur.com>
2015-04-29 19:03:48 -0500

    Added ability to visit all items by providing a nil pivot, and ability to visit in descending order.

--- treap.go
+++ treap.go
@@ -100,7 +100,7 @@ func (t *Treap) union(this *node, that *node) *node {
 	}
 }
 
-// Splits a treap into two treaps based on a split item "s".
+// Splits a treap into three treaps based on a split item "s".
 // The result tuple-3 means (left, X, right), where X is either...
 // nil - meaning the item s was not in the original treap.
 // non-nil - returning a treap with a single node having item s.
@@ -180,7 +180,7 @@ func (t *Treap) join(this *node, that *node) *node {
 
 type ItemVisitor func(i Item) bool
 
-// Visit items greater-than-or-equal to the pivot.
+// Visit items greater-than-or-equal to the pivot.  If the pivot is null, this visits all items in ascending order.
 func (t *Treap) VisitAscend(pivot Item, visitor ItemVisitor) {
 	t.visitAscend(t.root, pivot, visitor)
 }
@@ -189,13 +189,37 @@ func (t *Treap) visitAscend(n *node, pivot Item, visitor ItemVisitor) bool {
 	if n == nil {
 		return true
 	}
-	if t.compare(pivot, n.item) <= 0 {
+	if pivot == nil || t.compare(pivot, n.item) <= 0 {
 		if !t.visitAscend(n.left, pivot, visitor) {
 			return false
 		}
 		if !visitor(n.item) {
 			return false
 		}
+		//since n.right is > n.item by the comparison, we can speed up by not comparing
+		return t.visitAscend(n.right, nil, visitor)
 	}
 	return t.visitAscend(n.right, pivot, visitor)
+}
+
+// Visit items less-than-or-equal to the pivot.  If the pivot is null, this visits all items in descending order.
+func (t *Treap) VisitDescend(pivot Item, visitor ItemVisitor) {
+	t.visitDescend(t.root, pivot, visitor)
+}
+
+func (t *Treap) visitDescend(n *node, pivot Item, visitor ItemVisitor) bool {
+	if n == nil {
+		return true
+	}
+	if pivot == nil || t.compare(pivot, n.item) >= 0 {
+		if !t.visitDescend(n.right, pivot, visitor) {
+			return false
+		}
+		if !visitor(n.item) {
+			return false
+		}
+		//since n.left is < n.item by the comparison, we can speed up by not comparing
+		return t.visitDescend(n.left, nil, visitor)
+	}
+	return t.visitDescend(n.left, pivot, visitor)
 }
